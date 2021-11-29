619c9b6f5ad8bb86627826b49052da77e13867b5
김지훈 <jihoon951118@gmail.com>
2021-11-29 23:08:58 +0900

    variable_learning_1129

---
+++ 자바스크립트/variable.js
@@ -0,0 +1,16 @@
+// 1. Use strict
+// added is ES 5
+// use this for Vanila Javascript.
+'use strict';
+
+// 2. variable (변수)
+// let (added in ES6)
+
+let name = 'JJang Gu';
+console.log(name);
+
+name = 'hello';
+console.log(name);
+
+https://www.youtube.com/watch?v=OCCpGh4ujb8&list=PLv2d7VI9OotTVOL4QmPfvJWPJvkmv6h-2&index=4
+3:43초
